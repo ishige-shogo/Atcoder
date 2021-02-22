c549897cad4303c1d752347fabc57eb000ed1592
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-02-22 23:18:22 +0900

    atcoder D問題正解

--- atcoder192_pra.rb
+++ atcoder192_pra.rb
@@ -1,12 +1,37 @@
+
 X = gets.chomp
 M = gets.to_i
 
-#Xの逆順番
-x_lines = X.split(//).reverse
+x_max = X.to_s.split(//).max.to_i
+x_size = X.to_s.split(//).size
 
-#文字列X中の最大の数字: d
-x_max = X.split(//).max.to_i
 
+def base_sum(n, str)
+    x_line = str.to_s.split(//).reverse
+
+    sum = 0
+    x_line.each_with_index do |x, i|
+        sum += x.to_i * (n ** i)
+    end
+    sum
+end
+
+n_range = ((x_max + 1)..10**100)
+sum_line = n_range.bsearch{|r| base_sum(r, X) > M}.to_i
+
+if x_size == 1
+    if X.to_i > M
+        puts 0
+    else
+        puts 1
+    end
+else
+    puts sum_line - (x_max+1)
+end
+
+
+
+#------------------------------------------------------------
 ans = 0
 
 n = x_max + 1
@@ -21,6 +46,8 @@ while true do
 end
 
 puts ans
+
+
 
 data = (0..50000000)
 puts data.bsearch {|number| number > 40000000 }
