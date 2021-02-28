4e29935c3ef3dec7a5eaadbe01179e3b902d503b
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-02-28 18:27:47 +0900

    atcoder c

--- atcoder190_pra.rb
+++ atcoder190_pra.rb
@@ -33,3 +33,44 @@ if count==0
 else
     puts "Yes"
 end
+
+#------------------------------------------------------
+
+N, M = gets.chomp.split(" ").map(&:to_i);
+
+conditions = Array.new()
+
+M.times do |m|
+    a, b = gets.chomp.split(" ").map(&:to_i);
+    conditions[m] = [a, b]
+end
+
+K = gets.to_i
+
+people = Array.new()
+
+K.times do |k|
+    c, d = gets.chomp.split(" ").map(&:to_i);
+    people[k] = [c, d]
+end
+
+bit = Array.new()
+
+bit_line = [*0...(K**2)]
+
+bit_line.each do |b|
+    bit.append([b.to_s(2).split(//)])
+end
+
+ary = Array.new()
+
+
+bit.each do |b|
+    K.times do |k|
+        while true do
+            
+        end
+    end
+end
+
+
