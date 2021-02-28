0f001a195181b007990eee3039857de3ea04e076
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-02-28 12:12:43 +0900

    a,b問題まで

---
+++ atcoder190_pra.rb
@@ -0,0 +1,35 @@
+#5分
+A,B,C=gets.chomp.split(" ").map(&:to_i);
+if A > B || (A == B && C == 1)
+    puts Takahashi
+else
+    puts Aoki
+end
+
+#---------------------------------------------------------
+#20分
+N,S,D=gets.chomp.split(" ").map(&:to_i);
+
+magic = Array.new(N)
+
+N.times do |n|
+    x, y = gets.chomp.split(" ").map(&:to_i);
+    magic[n] = [x, y]
+end
+
+#カウントを0と置く
+count = 0
+0.upto(N-1) do |n|
+    #詠唱時間が規定より小さい且つダメージが規定より大きい場合、カウントを足して処理を終了
+    if ((magic[n][0] < S) && (magic[n][1] > D))
+        count += 1
+        break
+    end
+end
+
+#カウントが
+if count==0
+    puts "No"
+else
+    puts "Yes"
+end
