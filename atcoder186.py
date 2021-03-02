3adf7656c8dcd2d8d2a9d74511c94186c2828c91
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-02 19:29:40 +0900

    B問題

--- atcoder186.py
+++ atcoder186.py
@@ -3,3 +3,17 @@
 
 N, W = map(int, input().split())
 print(N//W)
+
+#----------------------------------------------------------
+# B問題
+
+H, W = map(int, input().split())
+A = [list(map(int,input().split())) for i in range(H)]
+# 作成したA配列の集合体を全て展開して１つの配列Bにする
+B = []
+for h in range(H):
+    for w in range(W):
+        num = A[h][w]
+        B.append(num)
+# 配列Bの数値の合計値から、Bの最小数値とBの要素数を掛けたものを引く
+print(sum(B) - (min(B) * len(B)))
