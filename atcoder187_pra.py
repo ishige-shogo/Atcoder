6e5e0edc5410ad2a43547e73498ac05f14e6a241
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-02 18:49:21 +0900

    C問題

--- atcoder187_pra.py
+++ atcoder187_pra.py
@@ -7,7 +7,6 @@ num_a = str(n)
 list_num_a = list(num_a)
 a_list = [int(s) for s in list_num_a]
 
-# 
 xy = []
 for i in range(n):
     x, y = map(int, input().split())
@@ -15,6 +14,7 @@ for i in range(n):
 
 #-------------------------------------------------
 # A問題
+
 A, B = map(int, input().split())
 num_a = str(A)
 num_b = str(B)
@@ -49,3 +49,95 @@ for s in range(n):
         if (-1 <= ans) and (ans <= 1):
             count += 1
 print(count)
+
+#-----------------------------------------------------
+# C問題
+#解法１：ごり押しパターン(処理時間エラー)
+n = int(input())
+ary = []
+for i in range(n):
+    s = str(input())
+    ary.append(s)
+
+a_line = []
+b_line = []
+
+for v in range(n):
+    if ary[v][0] == "!":
+        txt = ary[v][1:]
+        a_line.append(txt)
+    else:
+        b_line.append(ary[v])
+
+for u in a_line:
+    if u in b_line:
+        print(u)
+        exit()
+
+print("satisfiable")
+
+#上記の少し改良パターン(処理時間エラー)
+#処理時間を確認したら上記より長くなりました(改悪)
+
+n = int(input())
+ary = []
+for i in range(n):
+    s = str(input())
+    ary.append(s)
+
+a_line = []
+b_line = []
+
+for v in range(n):
+    if ary[v][0] == "!":
+        txt = ary[v][1:]
+        if txt in b_line:
+            print(txt)
+            exit()
+        a_line.append(txt)
+    else:
+        if ary[v] in a_line:
+            print(ary[v])
+            exit()
+        b_line.append(ary[v])
+
+print("satisfiable")
+
+#解法２：重複の文字列を排除する(処理時間エラー)
+
+n = int(input())
+ary = []
+for i in range(n):
+    s = str(input())
+    ary.append(s)
+
+wop = list(set(ary))
+m = len(wop)
+
+for v in range(m):
+    if wop[v][0] == "!":
+        wop[v] = wop[v][1:]
+
+
+for w in wop:
+    if wop.count(w) == 2:
+        print(w)
+        exit()
+
+print("satisfiable")
+
+
+#解法３：解法１と２のミックス簡単バージョン
+
+n = int(input())
+# 重複した要素を除いたリストを作成
+s = set(input() for i in range(n))
+# 先頭に！がついていた場合、それを除いた文字列が配列に存在するときに、その文字を出力(+プログラムの終了)
+for v in s:
+    if v[0] == "!":
+        if v[1:] in s:
+            print(v[1:])
+            exit()
+# 該当しない場合はこれを出力
+print("satisfiable")
+
