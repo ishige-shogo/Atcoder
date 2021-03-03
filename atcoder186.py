f9898548518dff534b1c19dd3d6745a248dbb97a
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-03 11:59:47 +0900

    D問題

--- atcoder186.py
+++ atcoder186.py
@@ -31,3 +31,31 @@ for i in range(N):
         m.append(i + 1)
 #全体からリストアップされた要素の個数を引く
 print(N - len(m))
+
+#------------------------------------------------------------
+# D問題
+
+#解法１：単純バージョン(実行時間エラー)
+N = int(input())
+A = list(map(int, input().split()))
+# １つずつ計算し、合計していく
+sum = 0
+for s in range(N):
+    for t in list(range(s+1,N)):
+        sum += abs(A[s] - A[t])
+print(sum)
+
+
+#解法２：試行回数を減らす(計算をまとめる)バージョン
+N = int(input())
+A = list(map(int, input().split()))
+#要素を小さい順に並べ替える
+a = sorted(A)
+# a = [D, C, B, A]のとき
+#(3A-0A)+(2B-1B)+(1C-2C)+(0D-3D)が問題の合計値になる
+sum = 0
+for s in range(N):
+    v = N - s - 1
+    num = (s * a[s]) - (s * a[v])
+    sum += num
+print(sum)
