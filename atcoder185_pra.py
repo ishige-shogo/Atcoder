99ed052b4bbc10cc9b42e201d05896475e221868
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-04 11:15:16 +0900

    C問題

--- atcoder185_pra.py
+++ atcoder185_pra.py
@@ -46,3 +46,29 @@ if battery <= 0:
 #バッテリー残量が0以下にならなかった場合(プログラムが終了しなかった)
 print("Yes")
 
+#--------------------------------------------------------
+# C問題
+
+L = int(input())
+#組み合わせの数：nCr コンビネーションのメソッド
+def cmb(n, r):
+    if n - r < r: r = n - r
+    if r == 0: return 1
+    if r == 1: return n
+    numerator = [n - r + k + 1 for k in range(r)]
+    denominator = [k + 1 for k in range(r)]
+    for p in range(2,r+1):
+        pivot = denominator[p - 1]
+        if pivot > 1:
+            offset = (n - r) % p
+            for k in range(p-1,r,p):
+                numerator[k - offset] /= pivot
+                denominator[k] /= pivot
+    result = 1
+    for k in range(r):
+        if numerator[k] > 1:
+            result *= int(numerator[k])
+    return result
+
+#植木算(仕切りを入れるパターン数を計算)
+print(cmb(L-1,11))
