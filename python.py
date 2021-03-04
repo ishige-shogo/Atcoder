5d08c06f0b2361227d6a330ee3013202d92a7041
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-04 11:15:06 +0900

    組み合わせの数

--- python.py
+++ python.py
@@ -49,3 +49,24 @@ print(hex(10)) # 0xa
 A = [2,4,1,3,5]
 a = sorted(A) #1,2,3,4,5
 a = sorted(A, reverse=True) #5,4,3,2,1
+
+
+# 組み合わせの数：nCr
+def cmb(n, r):
+    if n - r < r: r = n - r
+    if r == 0: return 1
+    if r == 1: return n
+    numerator = [n - r + k + 1 for k in range(r)]
+    denominator = [k + 1 for k in range(r)]
+    for p in range(2,r+1):
+        pivot = denominator[p - 1]
+        if pivot > 1:
+            offset = (n - r) % p
+            for k in range(p-1,r,p):
+                numerator[k - offset] /= pivot
+                denominator[k] /= pivot
+    result = 1
+    for k in range(r):
+        if numerator[k] > 1:
+            result *= int(numerator[k])
+    return result
