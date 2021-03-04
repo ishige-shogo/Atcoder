6dc1de4772a6e7ea646a96e4e2e13723eee18bbf
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-04 19:00:13 +0900

    C問題

--- atcoder184_pra.py
+++ atcoder184_pra.py
@@ -19,3 +19,25 @@ for i in range(n):
     elif s[i] == "x" and x != 0:
         x -= 1
 print(x)
+
+#---------------------------------------------------------
+# C問題
+
+# a, b, c, d = 座標(a, b)から座標(c, d)に駒を移動する
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+
+# どの場合でも3手以内に移動が可能
+# 移動前後の座標が同じの場合、駒を動かさずに終了(0手)
+if (a==c)and(b==d):
+    print(0)
+# 「斜線上」または「周囲３マス」の場合、1手で終了
+elif (a+b==c+d)or(a-b==c-d)or(abs(a-c)+abs(b-d)<=3):
+    print(1)
+# 「3マス移動後の3マス移動」または「3マス移動後の斜線上(斜線移動後の周囲3マス)」または「斜線移動後の斜線上」の場合、2手で終了
+elif (abs(a-c)+abs(b-d)<=6)or(abs(abs(a-c)-abs(b-d))<=3)or((abs(abs(a-c)-abs(b-d)))%2==0):
+    print(2)
+# 上記に合致しない場合は、必ず3手で終了する
+else:
+    print(3)
+
