642f921cf7ce0b0e841ecdc41978b23a81a014d8
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-05 22:43:11 +0900

    C問題

--- atcoder183_pra.py
+++ atcoder183_pra.py
@@ -25,4 +25,31 @@ else:
 #-------------------------------------------------------------
 # C問題
 
+# 全ての順序パターンを組み合わせで使用
+import itertools
 
+# n, k = 都市数, 想定移動時間
+n, k = map(int, input().split())
+# 各都市間の移動時間
+t = [list(map(int,input().split())) for i in range(n)]
+# 1から(n-1)までの連番を配列化
+city_number = list(range(1, n))
+# 全ての順序パターンをイテレート
+city = list(itertools.permutations(city_number))
+
+count = 0
+
+# tをもとに、各順列パターンでの移動時間を計算
+for i in city:
+    sum = 0
+    # 都市１から最初の都市jにかかる移動時間
+    sum += t[0][i[0]]
+    # 都市iから都市jにかかる移動時間
+    for s in range(n-2):
+        sum += t[i[s]][i[s+1]]
+    # 都市iから都市１にかかる移動時間
+    sum += t[i[n-2]][0]
+    # 移動時間の合計がkとなればカウントする
+    if sum == k:
+        count += 1
+print(count)
