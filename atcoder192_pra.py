08c8042667bef2561015dd591f7f3662687aa561
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-06 19:01:16 +0900

    B問題

--- atcoder192_pra.py
+++ atcoder192_pra.py
@@ -6,3 +6,19 @@ x = int(input())
 y = math.floor(x/100)
 
 print((y+1)*100-x)
+
+#------------------------------------------------------
+# B問題
+# 文字列ｓ
+s = str(input())
+# 文字列ｓを１文字ずつ検証。「奇数番目で大文字」か「偶数番目で小文字」の場合、判定はNoになる
+for i in range(len(s)):
+    if i % 2 == 0:
+        if s[i].isupper():
+            print("No")
+            exit()
+    else:
+        if s[i].islower():
+            print("No")
+            exit()
+print("Yes")
