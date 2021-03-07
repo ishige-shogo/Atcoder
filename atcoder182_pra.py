901b6ab6e4c882bb061667452936481c0fc05bb9
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-07 11:13:57 +0900

    B問題

--- atcoder182_pra.py
+++ atcoder182_pra.py
@@ -4,3 +4,23 @@ print(((2*A)+100)-B)
 
 #--------------------------------------------------------
 # B問題
+
+# 数列の数
+n = int(input())
+# 数列
+a = list(map(int, input().split()))
+
+# 最大のgcd度を記録
+gcd = 0
+for i in range(2, (max(a)+1)):
+    count = 0
+    # 割り切れたらカウントしていく
+    for s in a:
+        if s % i == 0:
+            count += 1
+    # 記録されているgcd度より大きい場合、ansとgcdを更新
+    if max(count, gcd) == count:
+        ans = i
+    gcd = max(count, gcd)
+
+print(ans)
