b3098210c6f85ab9f4f44fe8fb565341322692f4
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-09 14:45:37 +0900

    B問題

--- atcoder180_pra.py
+++ atcoder180_pra.py
@@ -2,5 +2,30 @@
 
 n, a, b = map(int, input().split())
 
-print(n-a+b)
+print(n - a + b)
+
+#----------------------------------------------
+# B問題
+
+n = int(input())
+
+x = list(map(int, input().split()))
+
+man = 0
+yuk = 0
+tyb = 0
+
+for i in x:
+    man += abs(i)
+
+for s in x:
+    yuk += s**2
+yuk = yuk ** (1/2)
+
+for v in x:
+    tyb = max(tyb, abs(v))
+
+print(man)
+print(yuk)
+print(tyb)
 
