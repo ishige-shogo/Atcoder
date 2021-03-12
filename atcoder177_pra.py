33cd4c2f7fa17755f1bef4679053a6aa4efc3f7c
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-12 18:39:22 +0900

    C問題

--- atcoder177_pra.py
+++ atcoder177_pra.py
@@ -27,3 +27,19 @@ for i in range(0, m+1):
         g += 1
     ans = min(ans, count)
 print(ans)
+
+#----------------------------------------------------------
+# C問題
+
+n = int(input())
+
+a = list(map(int, input().split()))
+
+total = 0
+ans = 0
+
+for i in range(n):
+    ans += a[i] * total
+    total += a[i]
+
+print(ans%(10**9 + 7))
