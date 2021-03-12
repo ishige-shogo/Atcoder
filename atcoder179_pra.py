9aa3871b80c85fd86c1df098651bac0585faca72
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-12 10:16:55 +0900

    B問題

--- atcoder179_pra.py
+++ atcoder179_pra.py
@@ -9,4 +9,20 @@ else:
 
 print(word)
 
-#
+#------------------------------------------------------
+# B問題
+
+n = int(input())
+
+# ぞろ目が出たらカウント。３回連続であればYESを出力し、終了。
+count = 0
+for i in range(n):
+    x, y = map(int, input().split())
+    if x == y:
+        count += 1
+    else:
+        count = 0
+    if count == 3:
+        print("Yes")
+        exit()
+print("No")
