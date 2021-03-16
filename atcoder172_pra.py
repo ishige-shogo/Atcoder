eafd1539b68fd7ea385ac90106e5350b569850a6
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-16 16:51:48 +0900

    C問題続き

--- atcoder172_pra.py
+++ atcoder172_pra.py
@@ -24,4 +24,18 @@ print(ans)
 #--------------------------------------------
 # C問題
 
+n, m, k = map(int, input().split())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a_len = len(a)
+b_len = len(b)
+
+a_con = 0
+b_con = 0
+ans = 0
+
+        
+
 
