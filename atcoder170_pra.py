e92feaf88ae2ff52aa8f9b8c4a16569e97be89bc
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-18 11:11:42 +0900

    C問題途中

--- atcoder170_pra.py
+++ atcoder170_pra.py
@@ -8,7 +8,7 @@ for i in range(x):
     if x[i] == 0:
         print(i+1)
 
-
+#-----------------------------------------------------
 # B問題
 
 x, y = map(int, input().split())
@@ -20,5 +20,22 @@ for i in range(x+1):
         exit()
 
 print("No")
+
+
+#----------------------------------------------------
+# C問題
+
+x, n = map(int, input().split())
+
+if n == 0:
+    print(x)
+    exit()
+
+p = list(map(int, input().split()))
+
+
+for i in range(101):
+    if i not in p:
+        
 
 
