4188b71e23df23abe3181d837f70e46fab848477
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-18 09:13:34 +0900

    C問題

--- atcoder171_pra.py
+++ atcoder171_pra.py
@@ -3,19 +3,30 @@
 n = int(input())
 
 ary = []
-while True:
+while n > 0:
+    n -= 1
     ary.append(n % 26)
     n = n // 26
-    if n < 26:
-        ary.append(n)
-        break
-
+    
 alph = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 ans = ""
 
-for i in ary:
+for i in reversed(ary):
     st = alph[i]
     ans += st
 
 print(ans)
+
+def main():
+    N = int(input())
+    result = []
+ 
+    while N > 0:
+        N -= 1
+        result.append(N % 26)
+        N //= 26
+    
+    print(''.join([chr(ord('a')+n) for n in result[::-1]]))
+ 
+main()
