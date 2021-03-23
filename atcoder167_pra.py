e6c080d01ff4f54e42856908eecf43f5ba3f461a
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-23 21:30:33 +0900

    B問題

--- atcoder167_pra.py
+++ atcoder167_pra.py
@@ -18,4 +18,21 @@ if s == t[:m]:
     print("Yes")
 else:
     print("No")
+#---------------------------------------------------
+# B問題
 
+a, b, c, k = map(int, input().split())
+
+if a >= k:
+    print(k)
+    exit()
+count = a
+k -= a
+if b >= k:
+    print(count)
+    exit()
+k -= b
+
+count -= k
+
+print(count)
