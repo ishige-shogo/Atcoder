ded11fab87c25f7a231ffd038a4f663bd83b5264
shigeshigeishige@gmail.com <shigeshigeishige@gmail.com>
2021-03-24 17:19:05 +0900

    B問題

--- atcoder166_pra.py
+++ atcoder166_pra.py
@@ -6,3 +6,17 @@ if s == "ABC":
     print("ARC")
 else:
     print("ABC")
+
+#------------------------------------------------------
+# B問題
+
+n, k = map(int, input().split())
+
+ary = []
+for _ in range(k):
+    d = int(input())
+    a = list(map(int, input().split()))
+    ary += a
+
+print(n - len(set(ary)))
+
