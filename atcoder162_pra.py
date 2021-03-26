a1958fce03448a3b98e12d7a3695770705009e37
ishige-shogo <shigeshigeishige@gmail.com>
2021-03-26 19:30:10 +0900

    C問題

--- atcoder162_pra.py
+++ atcoder162_pra.py
@@ -20,3 +20,29 @@ for i in range(1, n+1):
         ans += i
 print(ans)
 
+#-----------------------------------------------
+# C問題
+import itertools
+import math
+
+k = range(1, 4)
+
+m = itertools.product(k, repeat=3)
+
+ans = 0
+for i in m:
+    ans += math.gcd(i[0], i[1], i[2])
+print(ans)
+
+#------
+import math
+k = int(input())
+
+ans = 0
+for s in range(1, k+1):
+    for t in range(1, k+1):
+        st = math.gcd(s, t)
+        for u in range(1, k+1):
+            stu = math.gcd(st, u)
+            ans += stu
+print(ans)
