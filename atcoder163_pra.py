a1e76eb2acf61669fc2a915e2aa9c9dcb89947e0
ishige-shogo <shigeshigeishige@gmail.com>
2021-03-26 14:07:12 +0900

    B問題

--- atcoder163_pra.py
+++ atcoder163_pra.py
@@ -4,3 +4,17 @@ r = int(input())
 
 print(2*3.1415*r)
 
+#--------------------------------------------
+# B問題
+
+n, m = map(int, input().split())
+
+a = list(map(int, input().split()))
+
+s = sum(a)
+
+if n >= s:
+    print(n-s)
+else:
+    print(-1)
+    
