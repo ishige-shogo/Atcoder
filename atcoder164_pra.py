549a970ac5ba78aa128b892d890d97d9a1261d02
ishige-shogo <shigeshigeishige@gmail.com>
2021-03-26 12:00:40 +0900

    B問題

--- atcoder164_pra.py
+++ atcoder164_pra.py
@@ -9,3 +9,15 @@ else:
 
 #---------------------------------------------------
 # B問題
+
+a, b, c, d = map(int, input().split())
+
+while ((a > 0) and (c > 0)):
+    c -= b
+    if c <= 0:
+        print("Yes")
+        exit()
+    a -= d
+    if a <= 0:
+        print("No")
+        exit()
