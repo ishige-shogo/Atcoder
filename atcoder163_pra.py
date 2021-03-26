b666559a36a6b7f20e4a5cfb621273dcc6ee826c
ishige-shogo <shigeshigeishige@gmail.com>
2021-03-26 16:56:06 +0900

    C問題

--- atcoder163_pra.py
+++ atcoder163_pra.py
@@ -17,4 +17,45 @@ if n >= s:
     print(n-s)
 else:
     print(-1)
-    
+
+#----------------------------------------------
+# C問題
+# エラー
+n = int(input())
+a = list(map(int, input().split()))
+a_p = a + [0]
+
+p_a = [0] + a
+
+p = []
+h = a[0]
+c = 0
+for i in a_p:
+    if h == i:
+        c += 1
+    else:
+        p.append(c)
+        h = i
+        c = 1
+
+p += [0]
+
+f = -1
+v = 0
+for s in p_a:
+    if s != f:
+        print(p[v])
+        v += 1
+    else:
+        print(0)
+    f = s
+#-------
+
+import collections
+n = int(input())
+a = list(map(int, input().split()))
+
+ca = collections.Counter(a)
+
+for i in range(1, n+1):
+    print(ca[i])
