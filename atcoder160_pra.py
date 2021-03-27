0e0aa5e01f825b9f23bfade581fcf5ea8b3d7581
ishige-shogo <shigeshigeishige@gmail.com>
2021-03-27 12:10:04 +0900

    C問題

--- atcoder160_pra.py
+++ atcoder160_pra.py
@@ -9,3 +9,27 @@ else:
 
 # B問題
 
+x = int(input())
+
+a = x // 500
+y = x % 500
+
+b = y // 5
+
+print((a*1000) + (b*5))
+
+# C問題
+
+k, n = map(int, input().split())
+
+a = list(map(int, input().split()))
+
+a += [k]
+
+dis = [a[0]]
+
+for i in range(n):
+    dis.append(a[i+1] - a[i])
+
+
+print(min(a[n-1] - a[0], sum(dis) - max(dis)))
