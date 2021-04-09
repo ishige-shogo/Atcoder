09e87269ec646acf38f7e1f5b8d59f573fa3c4c5
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-09 20:39:46 +0900

    C問題

--- atcoder151_pra.py
+++ atcoder151_pra.py
@@ -15,3 +15,25 @@ elif (n*m - sum(a)) <= 0:
     print(0)
 else:
     print(n*m - sum(a))
+
+# C問題
+
+n, m = map(int, input().split())
+wa = [0]*n
+ac = [0]*n
+
+for _ in range(m):
+    p, s = map(str, input().split())
+    if ac[int(p)-1]==0 and s == "WA":
+        wa[int(p)-1] += 1
+    elif ac[int(p)-1]==0 and s == "AC":
+        ac[int(p)-1] = 1
+
+acc = 0
+waa = 0
+for i in range(n):
+    if ac[i] == 1:
+        waa += wa[i]
+        acc += 1
+
+print(acc, waa)
