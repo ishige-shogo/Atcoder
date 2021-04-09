aff4e04ca0cde0366456bae7356c10e11c2a7dfa
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-09 12:00:29 +0900

    D問題

--- atcoder154_pra.py
+++ atcoder154_pra.py
@@ -27,3 +27,17 @@ if len(a) == len(b):
     print("YES")
 else:
     print("NO")
+
+# D問題
+
+n, k = map(int, input().split())
+p = list(map(int, input().split()))
+ans = sum(p[:k])
+d = 0
+m = 0
+for i in range(n-k):
+    d += p[i+k] - p[i]
+    m = max(d, m)
+ans += m
+print((ans + k) / 2)
+
