016d346bd23519ad70114ddcdb21521d3ae1e8b3
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-09 11:05:24 +0900

    C問題

--- atcoder154_pra.py
+++ atcoder154_pra.py
@@ -14,5 +14,16 @@ print(a, b)
 # B問題
 
 s = input()
-
 print("x" * len(s))
+
+# C問題
+
+n = int(input())
+a = list(map(int, input().split()))
+
+b = set(a)
+
+if len(a) == len(b):
+    print("YES")
+else:
+    print("NO")
