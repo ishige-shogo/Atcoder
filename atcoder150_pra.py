5494f62af3a15ad4fbb49e1d16956e2effa0b37d
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-10 18:41:26 +0900

    B問題

--- atcoder150_pra.py
+++ atcoder150_pra.py
@@ -6,3 +6,16 @@ if x <= k*500:
     print("Yes")
 else:
     print("No")
+
+# B問題
+
+n = int(input())
+s = input()
+
+ans = 0
+for i in range(n-3):
+    v = s[i] + s[i+1] + s[i+2]
+    if v == "ABC":
+        ans += 1
+
+print(ans)
