7ed1d8bfc9c077c2e9b0f2d1036802ef4451d63e
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-10 19:10:52 +0900

    C問題

--- atcoder150_pra.py
+++ atcoder150_pra.py
@@ -13,9 +13,21 @@ n = int(input())
 s = input()
 
 ans = 0
-for i in range(n-3):
+for i in range(n-2):
     v = s[i] + s[i+1] + s[i+2]
     if v == "ABC":
         ans += 1
 
 print(ans)
+
+# C問題
+
+import itertools
+n = int(input())
+p = tuple(map(int, input().split()))
+q = tuple(map(int, input().split()))
+
+n_list = list(range(1, n+1))
+N = list(itertools.permutations(n_list))
+
+print(abs(N.index(p) - N.index(q)))
