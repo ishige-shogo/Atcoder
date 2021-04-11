73aab5939f229c31cde71a7d80dc29dabe316b23
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-11 18:03:14 +0900

    C問題

--- atcoder149_pra.py
+++ atcoder149_pra.py
@@ -14,3 +14,23 @@ elif (a+b) >= k:
     print(0, b-(k-a))
 else:
     print(0, 0)
+
+# C問題
+
+x = int(input())
+
+def is_prime(n):
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    h = n//2
+    for i in range(2, h+1):
+        if n % i == 0:
+            return False
+    return True
+
+for s in range(x, 100004):
+    if is_prime(s):
+        print(s)
+        exit()
