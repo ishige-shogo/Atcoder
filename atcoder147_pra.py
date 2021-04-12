a3a7229eac931bbf96dcd396d21e536f6f38c68d
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-12 18:56:37 +0900

    AB問題

--- atcoder147_pra.py
+++ atcoder147_pra.py
@@ -6,3 +6,16 @@ if a+b+c >= 22:
     print("bust")
 else:
     print("win")
+
+# B問題
+
+s = input()
+
+rs = s[::-1]
+
+ans = 0
+for i in range(len(s)):
+    if s[i] != rs[i]:
+        ans += 1
+
+print(ans/2)
