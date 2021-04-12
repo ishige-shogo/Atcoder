e9c3ad3b353e36662c349590453b4fb22ebf3e55
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-12 09:35:35 +0900

    BC問題

--- atcoder148_pra.py
+++ atcoder148_pra.py
@@ -9,3 +9,21 @@ y = [a, b]
 
 print(list(set(x) - set(y))[0])
 
+# B問題
+
+n = int(input())
+s, t = map(str, input().split())
+
+ans = ""
+
+for i in range(n):
+    ans += s[i] + t[i]
+
+print(ans)
+
+# C問題
+import math
+a, b = map(int, input().split())
+print(int((a*b)/math.gcd(a, b)))
+
+# D問題
