110105a79b25c01f064488b622cb6d82012d1cef
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-15 09:42:44 +0900

    B問題

--- atcoder145_pra.py
+++ atcoder145_pra.py
@@ -1,3 +1,20 @@
 # A問題
 r = int(input())
 print(r**2)
+
+# B問題
+
+n = int(input())
+s = input()
+
+if n % 2 != 0:
+    print("No")
+    exit()
+
+m = n//2
+
+if s[:m] == s[m:]:
+    print("Yes")
+else:
+    print("No")
+
