435c44e994b1d28413a2b774275c68ca5908b45b
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-15 10:45:12 +0900

    C問題

--- atcoder145_pra.py
+++ atcoder145_pra.py
@@ -18,3 +18,24 @@ if s[:m] == s[m:]:
 else:
     print("No")
 
+# C問題
+
+import itertools
+n = int(input())
+p = itertools.permutations(range(0,n), r=None)
+ary = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    ary.append([x, y])
+
+dist = 0
+count = 0
+for i in p:
+    for s in range(n-2):
+        a = i[s]
+        b = i[s+1]
+        dist += ((ary[a][0] - ary[b][0])**2 + (ary[a][1] - ary[b][1])**2)**(1/2)
+    count += 1
+print(dist / count)
+
+
