3eb94ddce7134258e0dfff9e89949923e0483b64
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-16 10:33:19 +0900

    BC問題

--- atcoder144_pra.py
+++ atcoder144_pra.py
@@ -5,3 +5,30 @@ if a <= 9 and b <= 9:
     print(a * b)
 else:
     print(-1)
+
+# B問題
+
+n = int(input())
+for i in range(1,10):
+    for s in range(1,10):
+        if n == i*s:
+            print("Yes")
+            exit()
+print("No")
+
+# C問題
+
+n = int(input())
+
+h = int(n**(0.5))
+
+ans = 10**13
+
+for i in range(1, h+1):
+    if n % i == 0:
+        m = n // i
+        s = (m + i) - 2
+        ans = min(s, ans)
+
+print(ans)
+
