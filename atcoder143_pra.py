a83144f1a40ba82c9c1ea6d2966805c3015e6c0c
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-17 15:47:42 +0900

    D問題

--- atcoder143_pra.py
+++ atcoder143_pra.py
@@ -40,3 +40,43 @@ for i in range(n):
         ans += 1
     color = s[i]
 print(ans)
+
+# D問題
+
+def get_unique_list(seq):
+    seen = []
+    return [x for x in seq if x not in seen and not seen.append(x)]
+
+n = int(input())
+l = list(map(int,input().split()))
+
+ans = []
+for a in range(n):
+    for b in range(a+1, n):
+        for c in range(b+1, n):
+            if (l[a] + l[b] > l[c]) and (l[b] + l[c] > l[a]) and (l[c] + l[a] > l[b]):
+                ans.append([l[a], l[b], l[c]])
+
+print(len(get_unique_list(ans)))
+
+#---------
+
+import bisect
+
+n = int(input())
+l = sorted(list(map(int,input().split())), reverse=True)
+
+ans = 0
+for a in range(n):
+    for b in range(a+1, n):
+        m = bisect.bisect_left(l, l[a]-l[b])
+        ans += m - b + 1
+
+print(ans)
+
+import bisect
+list1 = [1,2,3,4,5]
+value1 = 3
+print(bisect.bisect(list1, value1))
+
+
