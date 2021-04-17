9018e58eba12a7c0e88245c93a8f77cf2caa0f1d
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-17 10:18:31 +0900

    C問題

--- atcoder143_pra.py
+++ atcoder143_pra.py
@@ -26,3 +26,17 @@ for i in range(n):
     for s in range(i+1, n):
         ans += a[i]*a[s]
 print(ans)
+
+
+# C問題
+
+n = int(input())
+s = input()
+
+color = ""
+ans = 0
+for i in range(n):
+    if color != s[i]:
+        ans += 1
+    color = s[i]
+print(ans)
