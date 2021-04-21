bd0ab904141eb24f82b9e31af10d6462d4d1aaff
ishige-shogo <shigeshigeishige@gmail.com>
2021-04-21 20:34:17 +0900

    B問題

--- atcoder140_pra.py
+++ atcoder140_pra.py
@@ -8,3 +8,17 @@ print(n**3)
 
 # B問題
 
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = list(map(int, input().split()))
+
+ans = sum(b)
+pre = -1
+for i in a:
+    if pre + 1 == i:
+        ans += c[pre-1]
+    pre = i
+
+print(ans)
+
